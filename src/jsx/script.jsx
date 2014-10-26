886a3c5f214d20d19fce331331f0f21a59603e6f
alvinsj <email.to.alvin@gmail.com>
2014-10-26 20:01:17 +0700

    base

---
+++ src/jsx/script.jsx
@@ -0,0 +1,85 @@
+$(function(){
+
+	var Pic = React.createClass({
+	  render: function() {
+	    return (
+	      <div className="pic">
+	      	<blockquote className="picText" >
+	        	{this.props.text}
+	        	<div className="picAuthor">
+	          		<small>{this.props.author}</small>
+	        	</div>
+	        </blockquote>
+	        
+	        {this.props.children}
+	      </div>
+	    );
+	  }
+	});
+
+	var PicList = React.createClass({
+	  render: function() {
+	    var picNodes = this.props.data.
+	    filter(function(pic){ return pic.image_source.indexOf("instagram") > -1 }).
+	    map(function (pic) {
+	      return (
+	        <Pic author={pic.user} text={pic.tweet}>
+	          <div><img src={pic.image_source+"media?size=l"} className="image" /></div>
+	        </Pic>
+	      );
+	    });
+	    return (
+	      <div className="picList">
+	        {picNodes}
+	      </div>
+	    );
+	  }
+	});
+
+	var PicForm = React.createClass({
+	  render: function() {
+	    return (
+	      <div className="picForm">
+	       
+	      </div>
+	    );
+	  }
+	});
+
+
+	var PicBox = React.createClass({
+	  getInitialState: function() {
+	    return {data: []};
+	  },
+	  loadPicsFromServer: function() {
+	    $.ajax({
+	      url: this.props.url,
+	      dataType: 'json',
+	      success: function(data) {
+	        this.setState({data: data});
+	      }.bind(this),
+	      error: function(xhr, status, err) {
+	        console.error(this.props.url, status, err.toString());
+	      }.bind(this)
+	    });
+	  },
+	  componentDidMount: function() {
+	    this.loadPicsFromServer();
+	    //setInterval(this.loadPicsFromServer, this.props.pollInterval);
+	  },
+	  render: function() {
+	    return (
+	      <div className="picBox">
+	        <h1>#beautifulsky</h1>
+	        <PicList data={this.state.data} />
+	        <PicForm />
+	      </div>
+	    );
+	  }
+	});
+
+	React.renderComponent(
+	  <PicBox url="/images" pollInterval={2000} />,
+	  document.getElementById('content')
+	);
+})
