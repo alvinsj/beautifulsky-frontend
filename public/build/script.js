b0f223614b0544c4511b9c9a6f3abd1a0786f51f
alvinsj <email.to.alvin@gmail.com>
2015-01-23 03:57:24 +0700

    uses flex, autolinker, and cleanup

--- public/build/script.js
+++ public/build/script.js
@@ -1,84 +1,94 @@
 /** @jsx React.DOM */
-$(function(){
+function timeSince(date) {
 
-	var Pic = React.createClass({displayName: 'Pic',
-	  render: function() {
-	    return (
-	      React.DOM.div({className: "pic"}, 
-	      	React.DOM.blockquote({className: "picText"}, 
-	        	this.props.text, 
-	        	React.DOM.div({className: "picAuthor"}, 
-	          		React.DOM.small(null, this.props.author, ", ", moment(this.props.created).fromNow())
-	        	)
-	        ), 
-	        
-	        this.props.children
-	      )
-	    );
-	  }
-	});
+    var seconds = Math.floor((new Date() - date) / 1000);
 
-	var PicList = React.createClass({displayName: 'PicList',
-	  render: function() {
-	    var picNodes = this.props.data.
-	    filter(function(pic){ return pic.image_source.indexOf("instagram") > -1 }).
-	    map(function (pic) {
-	      return (
-	        Pic({author: pic.user, text: pic.tweet, created: pic.created}, 
-	          React.DOM.div(null, React.DOM.img({src: pic.image_source+"media?size=l", className: "image"}))
-	        )
-	      );
-	    });
-	    return (
-	      React.DOM.div({className: "picList"}, 
-	        picNodes
-	      )
-	    );
-	  }
-	});
+    var interval = Math.floor(seconds / 31536000);
 
-	var PicForm = React.createClass({displayName: 'PicForm',
-	  render: function() {
-	    return (
-	      React.DOM.div({className: "picForm"}
-	       
-	      )
-	    );
-	  }
-	});
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
 
+var Pic = React.createClass({displayName: 'Pic',
+  render: function() {
+    return (
+      React.DOM.div({className: "pic"}, 
+      	React.DOM.blockquote({className: "picText"}, 
+        	React.DOM.div({dangerouslySetInnerHTML: {__html: Autolinker.link(this.props.text)}}), 
+        	React.DOM.div({className: "picAuthor"}, 
+          		React.DOM.small(null, 
+					React.DOM.span({dangerouslySetInnerHTML: {__html: Autolinker.link(this.props.author)}}), 
+					", ", timeSince(new Date(this.props.created)), " ago")
+        	)
+        ), 
 
-	var PicBox = React.createClass({displayName: 'PicBox',
-	  getInitialState: function() {
-	    return {data: []};
-	  },
+        this.props.children
+      )
+    );
+  }
+});
 
-	  loadPicsFromServer: function() {
-        	oboe('tweets')
-        	.node('!.*', function(pic){           	
-           		var arr = this.state.data;
-           		arr.push(pic)
-            		this.setState({data: arr});
-        	}.bind(this));
-	  },
+var PicList = React.createClass({displayName: 'PicList',
+  render: function() {
+    var picNodes = this.props.data.
+    filter(function(pic){ return pic.image_source.indexOf("instagram") > -1 }).
+    map(function (pic, index) {
+      return (
+        Pic({key: index, author: pic.user, text: pic.tweet, created: pic.created}, 
+          React.DOM.div(null, React.DOM.img({src: pic.image_source+"media?size=l", className: "image"}))
+        )
+      );
+    });
+    return (
+      React.DOM.div({className: "picList", 'data-flex': "horizontal wrap grow"}, 
+        picNodes
+      )
+    );
+  }
+});
 
-	  componentDidMount: function() {
-	    this.loadPicsFromServer();
-	  },
+var BeautifulSky = React.createClass({displayName: 'BeautifulSky',
+  getInitialState: function() {
+    return {data: []};
+  },
 
-	  render: function() {
-	    return (
-	      React.DOM.div({className: "picBox"}, 
-	        React.DOM.h1(null, "#beautifulsky"), 
-	        PicList({data: this.state.data}), 
-	        PicForm(null)
-	      )
-	    );
-	  }
-	});
+  loadPicsFromServer: function() {
+    	oboe('tweets')
+    	.node('!.*', function(pic){
+       		var arr = this.state.data;
+       		arr.push(pic)
+        		this.setState({data: arr});
+    	}.bind(this));
+  },
 
-	React.renderComponent(
-	  PicBox({url: "/images", pollInterval: 2000}),
-	  document.getElementById('content')
-	);
-})
+  componentDidMount: function() {
+    this.loadPicsFromServer();
+  },
+
+  render: function() {
+    return PicList({data: this.state.data});
+  }
+});
+
+React.renderComponent(
+  BeautifulSky({url: "/images", pollInterval: 2000}),
+  document.getElementById('content')
+);
--- src/js/script.jsx
+++ src/js/script.jsx
@@ -1,83 +1,93 @@
-$(function(){
+function timeSince(date) {
 
-	var Pic = React.createClass({
-	  render: function() {
-	    return (
-	      <div className="pic">
-	      	<blockquote className="picText" >
-	        	{this.props.text}
-	        	<div className="picAuthor">
-	          		<small>{this.props.author}, {moment(this.props.created).fromNow()}</small>
-	        	</div>
-	        </blockquote>
-	        
-	        {this.props.children}
-	      </div>
-	    );
-	  }
-	});
+    var seconds = Math.floor((new Date() - date) / 1000);
 
-	var PicList = React.createClass({
-	  render: function() {
-	    var picNodes = this.props.data.
-	    filter(function(pic){ return pic.image_source.indexOf("instagram") > -1 }).
-	    map(function (pic) {
-	      return (
-	        <Pic author={pic.user} text={pic.tweet} created={pic.created}>
-	          <div><img src={pic.image_source+"media?size=l"} className="image" /></div>
-	        </Pic>
-	      );
-	    });
-	    return (
-	      <div className="picList">
-	        {picNodes}
-	      </div>
-	    );
-	  }
-	});
+    var interval = Math.floor(seconds / 31536000);
 
-	var PicForm = React.createClass({
-	  render: function() {
-	    return (
-	      <div className="picForm">
-	       
-	      </div>
-	    );
-	  }
-	});
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
 
+var Pic = React.createClass({
+  render: function() {
+    return (
+      <div className="pic">
+      	<blockquote className="picText" >
+        	<div dangerouslySetInnerHTML={{__html: Autolinker.link(this.props.text)}} />
+        	<div className="picAuthor">
+          		<small>
+					<span dangerouslySetInnerHTML={{__html: Autolinker.link(this.props.author)}} />
+					, {timeSince(new Date(this.props.created))} ago</small>
+        	</div>
+        </blockquote>
 
-	var PicBox = React.createClass({
-	  getInitialState: function() {
-	    return {data: []};
-	  },
+        {this.props.children}
+      </div>
+    );
+  }
+});
 
-	  loadPicsFromServer: function() {
-        	oboe('tweets')
-        	.node('!.*', function(pic){           	
-           		var arr = this.state.data;
-           		arr.push(pic)
-            		this.setState({data: arr});
-        	}.bind(this));
-	  },
+var PicList = React.createClass({
+  render: function() {
+    var picNodes = this.props.data.
+    filter(function(pic){ return pic.image_source.indexOf("instagram") > -1 }).
+    map(function (pic, index) {
+      return (
+        <Pic key={index} author={pic.user} text={pic.tweet} created={pic.created} >
+          <div><img src={pic.image_source+"media?size=l"} className="image" /></div>
+        </Pic>
+      );
+    });
+    return (
+      <div className="picList" data-flex="horizontal wrap grow">
+        {picNodes}
+      </div>
+    );
+  }
+});
 
-	  componentDidMount: function() {
-	    this.loadPicsFromServer();
-	  },
+var BeautifulSky = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
 
-	  render: function() {
-	    return (
-	      <div className="picBox">
-	        <h1>#beautifulsky</h1>
-	        <PicList data={this.state.data} />
-	        <PicForm />
-	      </div>
-	    );
-	  }
-	});
+  loadPicsFromServer: function() {
+    	oboe('tweets')
+    	.node('!.*', function(pic){
+       		var arr = this.state.data;
+       		arr.push(pic)
+        		this.setState({data: arr});
+    	}.bind(this));
+  },
 
-	React.renderComponent(
-	  <PicBox url="/images" pollInterval={2000} />,
-	  document.getElementById('content')
-	);
-})
+  componentDidMount: function() {
+    this.loadPicsFromServer();
+  },
+
+  render: function() {
+    return <PicList data={this.state.data} />;
+  }
+});
+
+React.renderComponent(
+  <BeautifulSky url="/images" pollInterval={2000} />,
+  document.getElementById('content')
+);
